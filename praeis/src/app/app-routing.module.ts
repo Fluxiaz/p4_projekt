bf26f489785385da9f89c27f53fccc7c215a4dd0
SyrupSniffer <generalptj@gmail.com>
2021-02-17 05:02:11 +0100

    Component and Routing
    Components created
    Routing setup done and awaiting link creations.

--- praeis/src/app/app-routing.module.ts
+++ praeis/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { BasketComponent } from './basket/basket.component';
+import { ItemsComponent } from './items/items.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path: 'AppComponet', component: AppComponent },
+  { path: 'AdministrationComponet', component: AdministrationComponent },
+  { path: 'BasketComponent', component: BasketComponent },
+  { path: 'ItemsComponent', component: ItemsComponent },
+  { path: 'ProfileComnponent', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- praeis/src/app/app.module.ts
+++ praeis/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BasketComponent } from './basket/basket.component';
+import { ItemsComponent } from './items/items.component';
+import { AdministrationComponent } from './administration/administration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    BasketComponent,
+    ItemsComponent,
+    AdministrationComponent
   ],
   imports: [
     BrowserModule,
