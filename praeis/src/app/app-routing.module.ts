b1785641b8618b2e3596b964ee9c27a2699feb59
SyrupSniffer <generalptj@gmail.com>
2021-03-08 10:14:50 +0100

    Routing
    Routing sat op, og burde virke.

--- praeis/src/app/app-routing.module.ts
+++ praeis/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { BasketComponent } from './basket/basket.component';
 import { ItemsComponent } from './items/items.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: 'AppComponet', component: AppComponent },
   { path: 'AdministrationComponet', component: AdministrationComponent },
   { path: 'BasketComponent', component: BasketComponent },
   { path: 'ItemsComponent', component: ItemsComponent },
@@ -18,8 +16,7 @@ const routes: Routes = [
 @NgModule({ 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],   
-
-
 })
 
 export class AppRoutingModule { }
+export const routingComponets = [AdministrationComponent, BasketComponent, ItemsComponent, ProfileComponent]
--- praeis/src/app/app.module.ts
+++ praeis/src/app/app.module.ts
@@ -2,20 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponets } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BasketComponent } from './basket/basket.component';
-import { ItemsComponent } from './items/items.component';
-import { AdministrationComponent } from './administration/administration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
-    BasketComponent,
-    ItemsComponent,
-    AdministrationComponent
+    routingComponets
   ],
   imports: [
     BrowserModule,
