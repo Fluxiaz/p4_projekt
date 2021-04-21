1755f8f15b7e5ce8c93285acc5f9210427109eb4
EmirhanYilmaz <ey001500@gmail.com>
2021-04-21 14:39:17 +0200

    Lots of changes, profile n shi

--- praeis/src/app/app-routing.module.ts
+++ praeis/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { WelcomeComponent } from './welcome/welcome.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { BasketComponent } from './basket/basket.component';
 import { ItemsComponent } from './items/items.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AppComponent } from './app.component'
 
 const routes: Routes = [
   { path: 'AdministrationComponet', component: AdministrationComponent },
   { path: 'BasketComponent', component: BasketComponent },
   { path: 'ItemsComponent', component: ItemsComponent },
   { path: 'ProfileComnponent', component: ProfileComponent },
+  { path: 'AppComponent', component: AppComponent },
+  { path: 'WelcomeComponent', component: WelcomeComponent },
+
+
 ];
 
 @NgModule({ 
@@ -19,4 +25,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule { }
-export const routingComponets = [AdministrationComponent, BasketComponent, ItemsComponent, ProfileComponent, ]
+export const routingComponets = [AdministrationComponent, BasketComponent, ItemsComponent, ProfileComponent, AppComponent, WelcomeComponent ]
